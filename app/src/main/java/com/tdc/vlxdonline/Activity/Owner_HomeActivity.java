2024c5de22f7bce15bfc4144c5091389d7f3e54a
Nguyen Van Linh <linhvng5420@gmail.com>
2024-10-17 15:49:52 +0700

    Bat Dau Code Quan Ly Nhan Vien: Tao Fragment

--- app/src/main/java/com/tdc/vlxdonline/Activity/Owner_HomeActivity.java
+++ app/src/main/java/com/tdc/vlxdonline/Activity/Owner_HomeActivity.java
@@ -2,25 +2,49 @@ package com.tdc.vlxdonline.Activity;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.tdc.vlxdonline.R;
+import com.tdc.vlxdonline.databinding.ActivityOwnerHomeBinding;
 
 public class Owner_HomeActivity extends AppCompatActivity {
+    //1. Declare variables
+    ActivityOwnerHomeBinding ownerHomeBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_owner_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        ownerHomeBinding = ActivityOwnerHomeBinding.inflate(getLayoutInflater());
+        setContentView(ownerHomeBinding.getRoot());
+
+        //2. Set Fragment mặc định (Màn hình mặc định)
+
+
+        //3. Bắt sự kiện
+        EventNavigationBottom();
+    }
+
+    // Bắt sự kiện nhấn Navbar Bottom
+    private void EventNavigationBottom() {
+        ownerHomeBinding.navOwner.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_home_nhanvien) {
+                ReplaceFragment(new Owner_StaffFragment());
+            } else if (item.getItemId() == R.id.nav_home_dashboard) {
+                ReplaceFragment(new Fragment());
+            } else if (item.getItemId() == R.id.nav_home_donhang) {
+                
+            }
+            return true;
         });
+    }
+
+    private void ReplaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_owner, fragment);
+        fragmentTransaction.commit();
     }
 }
