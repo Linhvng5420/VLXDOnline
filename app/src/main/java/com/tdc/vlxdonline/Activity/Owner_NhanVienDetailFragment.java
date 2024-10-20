97b071dac8d1591f7598c9271011dfb8590ef88c
Nguyen Van Linh <linhvng5420@gmail.com>
2024-10-20 23:17:55 +0700

    Thay đổi giao diện (Bỏ Ảnh, avatar)

--- app/src/main/java/com/tdc/vlxdonline/Activity/Owner_NhanVienDetailFragment.java
+++ app/src/main/java/com/tdc/vlxdonline/Activity/Owner_NhanVienDetailFragment.java
@@ -1,16 +1,10 @@
 package com.tdc.vlxdonline.Activity;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -19,8 +13,6 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.tdc.vlxdonline.Model.NhanVien;
@@ -38,21 +30,9 @@ public class Owner_NhanVienDetailFragment extends Fragment {
     private Spinner spinnerChucVu;
     private String[] chucVuArray = {"Kho", "Giao Hàng"};
 
-    // Mã yêu cầu cho việc chọn ảnh
-    private static final int PICK_IMAGE_AVATAR = 1;
-    private static final int PICK_IMAGE_FRONT_ID = 2;
-    private static final int PICK_IMAGE_BACK_ID = 3;
-
-    // Uri để lưu trữ đường dẫn đến ảnh được chọn
-    private Uri avatarUri, frontIdUri, backIdUri;
-
-    // Các ImageView đại diện cho ảnh avatar, ảnh CMND trước và sau
-    private ImageView ivAvatar, ivFrontId, ivBackId;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        checkPermissions(); // Kiểm tra quyền truy cập khi fragment được tạo
     }
 
     @Override
@@ -65,11 +45,6 @@ public class Owner_NhanVienDetailFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        // Gán các ImageView từ View Binding
-        ivAvatar = nhanvienDetailBinding.ivAvatar;
-        ivFrontId = nhanvienDetailBinding.ivFrontId;
-        ivBackId = nhanvienDetailBinding.ivBackId;
 
         // Lấy Spinner từ View Binding
         spinnerChucVu = nhanvienDetailBinding.spinnerChucVu;
@@ -91,6 +66,9 @@ public class Owner_NhanVienDetailFragment extends Fragment {
 
         // Thiết lập sự kiện cho nút Xóa
         setupDeleteButton();
+
+        // Thiết lập sự kiện cho nút Hủy
+        setupCancelButton();
     }
 
     // Thiết lập Toolbar và điều hướng
@@ -145,15 +123,11 @@ public class Owner_NhanVienDetailFragment extends Fragment {
             nhanvienDetailBinding.spinnerChucVu.setVisibility(View.VISIBLE);
             nhanvienDetailBinding.tvChucVu.setVisibility(View.VISIBLE);
 
-            // Hiển thị nút Lưu Lại, nút Xóa. Ẩn nút Chỉnh Sửa
+            // Hiển thị nút Lưu Lại, Xóa, Hủy. Ẩn nút Chỉnh Sửa
             nhanvienDetailBinding.btnLuuLai.setVisibility(View.VISIBLE);
-            nhanvienDetailBinding.btnChinhSua.setVisibility(View.INVISIBLE);
             nhanvienDetailBinding.btnXoa.setVisibility(View.VISIBLE);
-
-            // Cho phép người dùng chọn ảnh khi nhấn vào các ImageView
-            ivAvatar.setOnClickListener(v1 -> openImagePicker(PICK_IMAGE_AVATAR));
-            ivFrontId.setOnClickListener(v1 -> openImagePicker(PICK_IMAGE_FRONT_ID));
-            ivBackId.setOnClickListener(v1 -> openImagePicker(PICK_IMAGE_BACK_ID));
+            nhanvienDetailBinding.btnHuy.setVisibility(View.VISIBLE);
+            nhanvienDetailBinding.btnChinhSua.setVisibility(View.INVISIBLE);
         });
     }
 
@@ -200,15 +174,12 @@ public class Owner_NhanVienDetailFragment extends Fragment {
                         nhanvienDetailBinding.spinnerChucVu.setVisibility(View.INVISIBLE);
                         nhanvienDetailBinding.tvChucVu.setVisibility(View.INVISIBLE);
 
-                        // Ẩn nút Lưu Lại, nút xóa sau khi lưu
+                        // Ẩn nút Lưu Lại, Xóa, Hủy và Hiển thị nút Sửa sau khi lưu
                         nhanvienDetailBinding.btnLuuLai.setVisibility(View.INVISIBLE);
-                        nhanvienDetailBinding.btnChinhSua.setVisibility(View.VISIBLE);
+                        nhanvienDetailBinding.btnHuy.setVisibility(View.VISIBLE);
                         nhanvienDetailBinding.btnXoa.setVisibility(View.INVISIBLE);
-
-                        // Vô hiệu hóa sự kiện onClick của các ImageView (ngăn người dùng chọn ảnh sau khi lưu)
-                        ivAvatar.setOnClickListener(null);
-                        ivFrontId.setOnClickListener(null);
-                        ivBackId.setOnClickListener(null);
+                        nhanvienDetailBinding.btnHuy.setVisibility(View.INVISIBLE);
+                        nhanvienDetailBinding.btnChinhSua.setVisibility(View.VISIBLE);
 
                         // Cập nhật giao diện với thông tin mới
                         nhanvienDetailBinding.etChucVu.setText(selectedNhanVien.getChucVu() == 0 ? "Kho" : "Giao Hàng");
@@ -221,47 +192,35 @@ public class Owner_NhanVienDetailFragment extends Fragment {
         });
     }
 
-    // Mở trình chọn ảnh
-    private void openImagePicker(int requestCode) {
-        // ACTION_GET_CONTENT: cho phép chọn một tệp từ bất kỳ nguồn nào, bao gồm cả trình quản lý tệp và các ứng dụng khác.
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*"); // Chỉ định loại tệp là hình ảnh
-        intent.addCategory(Intent.CATEGORY_OPENABLE); // Thêm thể loại này để đảm bảo rằng trình quản lý tệp hiển thị các tệp có thể mở được.
-        startActivityForResult(Intent.createChooser(intent, "Chọn ảnh"), requestCode); // Mở trình hộp thoại chọn ảnh
-    }
+    // Thiết lập sự kiện cho nút Hủy
+    private void setupCancelButton() {
+        nhanvienDetailBinding.btnHuy.setOnClickListener(v -> {
+            // Tạo hộp thoại xác nhận
+            new AlertDialog.Builder(getContext())
+                    .setTitle("Xác Nhận")
+                    .setMessage("Bạn có chắc chắn muốn hủy thay đổi không?")
+                    .setPositiveButton("Hủy", (dialog, which) -> {
 
-    // Xử lý ảnh sau khi chọn
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // Kiểm tra kết quả trả về từ hoạt động chọn ảnh
-        if (resultCode == Activity.RESULT_OK && data != null) {
-            // Lấy Uri của ảnh đã chọn từ Intent data
-            Uri selectedImageUri = data.getData();
+                        // Vô hiệu hóa các trường chỉnh sửa sau khi hủy
+                        nhanvienDetailBinding.etTenNhanVien.setEnabled(false);
+                        nhanvienDetailBinding.etSDT.setEnabled(false);
+                        nhanvienDetailBinding.etEmail.setEnabled(false);
 
-            // Dựa vào mã yêu cầu để xác định ảnh nào đã được chọn
-            if (requestCode == PICK_IMAGE_AVATAR) {
-                // Lưu Uri ảnh đại diện và hiển thị ảnh trong ImageView
-                avatarUri = selectedImageUri;
-                ivAvatar.setImageURI(avatarUri); // Hiển thị ảnh đại diện
-            } else if (requestCode == PICK_IMAGE_FRONT_ID) {
-                // Lưu Uri ảnh CMND trước và hiển thị ảnh trong ImageView
-                frontIdUri = selectedImageUri;
-                ivFrontId.setImageURI(frontIdUri); // Hiển thị ảnh CMND trước
-            } else if (requestCode == PICK_IMAGE_BACK_ID) {
-                // Lưu Uri ảnh CMND sau và hiển thị ảnh trong ImageView
-                backIdUri = selectedImageUri;
-                ivBackId.setImageURI(backIdUri); // Hiển thị ảnh CMND sau
-            }
-        }
-    }
+                        // Ẩn Spinner và hiển thị TextView cho chức vụ
+                        nhanvienDetailBinding.tilChucVu.setVisibility(View.VISIBLE);
+                        nhanvienDetailBinding.etChucVu.setVisibility(View.VISIBLE);
+                        nhanvienDetailBinding.spinnerChucVu.setVisibility(View.INVISIBLE);
+                        nhanvienDetailBinding.tvChucVu.setVisibility(View.INVISIBLE);
 
-    // Kiểm tra và yêu cầu quyền truy cập
-    private void checkPermissions() {
-        // Kiểm tra xem ứng dụng có quyền truy cập vào bộ nhớ ngoài hay không
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            // Nếu chưa có quyền, yêu cầu quyền truy cập từ người dùng
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PICK_IMAGE_AVATAR);
-        }
+                        // Ẩn nút Lưu Lại, Xóa, Hủy và Hiển thị nút Sửa sau khi Hủy
+                        nhanvienDetailBinding.btnLuuLai.setVisibility(View.INVISIBLE);
+                        nhanvienDetailBinding.btnHuy.setVisibility(View.VISIBLE);
+                        nhanvienDetailBinding.btnXoa.setVisibility(View.INVISIBLE);
+                        nhanvienDetailBinding.btnHuy.setVisibility(View.INVISIBLE);
+                        nhanvienDetailBinding.btnChinhSua.setVisibility(View.VISIBLE);
+                    })
+                    .setNegativeButton("Không Hủy", null) // Hiển thị hộp thoại
+                    .show();
+        });
     }
 }
