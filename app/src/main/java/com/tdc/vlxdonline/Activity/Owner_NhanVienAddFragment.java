40cda3c3ef00bbf2a6f61f5efb1c0350cd5053f3
Nguyen Van Linh <linhvng5420@gmail.com>
2024-10-21 20:25:08 +0700

    Chuẩn bị Tạo Mới Nhân Viên

---
+++ app/src/main/java/com/tdc/vlxdonline/Activity/Owner_NhanVienAddFragment.java
@@ -0,0 +1,27 @@
+package com.tdc.vlxdonline.Activity;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tdc.vlxdonline.R;
+
+public class Owner_NhanVienAddFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_owner_nhan_vien_add, container, false);
+    }
+}
